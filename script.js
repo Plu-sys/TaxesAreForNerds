2a20aeeae703b1d706b665851d4fae387639f37c
Chance <68149062+pyralspite-ch0nce@users.noreply.github.com>
2020-10-30 15:52:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //playerData for saves.
 var playerData = [
-     money = 10,
-     AutoPrinters = 0
+  money = 10,
+  AutoPrinters = 0
 ]
 
 //Displaying The total money earned.
@@ -18,12 +18,13 @@ function ThemeToggle() {
     var element = document.body;
     element.classList.toggle("theme-toggle");
   };
+
   var Auto_Print;
   function AutoPrint() {
     Auto_Print = setInterval(getrich, 1000);
   };
 
-//reduces money by cost of printer and increments printer (something here is fucked)
+//reduces money by cost of printer and increments printer 
 function BuyAutoPrinters() {
     var AutoPrinterCost = 10;
     if (money >= AutoPrinterCost) {
@@ -31,21 +32,21 @@ function BuyAutoPrinters() {
       AutoPrinters++;
       document.getElementById("TotalMoney").innerHTML = money;
       document.getElementById("TotalAutoPrinters").innerHTML = AutoPrinters;
-    var NewAutoPrinterCost = math.pow(1.5 , AutoPrinterCost);
-    document.getElementById("AutoPrinterCost").innerHTML = NewAutoPrinterCost
+      console.log(AutoPrinters);
+      console.log(money);
+    
+    var NewAutoPrinterCost = Math.floor(Math.pow(AutoPrinterCost, 1.1));
+    document.getElementById("AutoPrinterCost").innerHTML = NewAutoPrinterCost;
+    }
+    else {
+      money = money;
+      AutoPrinters = AutoPrinters
     }
 }
-
-  //Cost of the auto printer
-  var AutoPrinterCost = 10
-  function AutoPrinterCostFunc() {
-  AutoPrinterCost == math.floor(math.pow(1.5 , AutoPrinterCost));
-  }
 
 //Buys an Auto Printer and activates it
  function BuyAndPrint() {
   BuyAutoPrinters();
-  AutoPrint();
  };
 
 
