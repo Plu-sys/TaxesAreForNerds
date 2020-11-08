e7bdad3cfbc8a8c35d33bebdbdccd98b9b29557d
Chance <68149062+pyralspite-ch0nce@users.noreply.github.com>
2020-11-08 13:51:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,50 +4,36 @@ var playerData = [
   AutoPrinters = 0
 ]
 
-//Displaying The total money earned.
+//Displaying stuff.
 document.getElementById("TotalMoney").innerHTML = money;
 
 //Incrementing the money and illegal money.
 function getrich() {
-    money++;
-    document.getElementById("TotalMoney").innerHTML = money;
+  money++;
+  document.getElementById("TotalMoney").innerHTML = money;
 }
 
-//Toggles between Dark and Light mode. (ew light mode)
+//Toggles between Dark and Light mode. (ew light mode).
 function ThemeToggle() {
-    var element = document.body;
-    element.classList.toggle("theme-toggle");
-  };
-
-  var Auto_Print;
-  function AutoPrint() {
-    Auto_Print = setInterval(getrich, 1000);
-  };
-
-//reduces money by cost of printer and increments printer 
-function BuyAutoPrinters() {
-    var AutoPrinterCost = 10;
-    if (money >= AutoPrinterCost) {
-      money = money -= AutoPrinterCost;
-      AutoPrinters++;
-      document.getElementById("TotalMoney").innerHTML = money;
-      document.getElementById("TotalAutoPrinters").innerHTML = AutoPrinters;
-      console.log(AutoPrinters);
-      console.log(money);
-    
-    var NewAutoPrinterCost = Math.floor(Math.pow(AutoPrinterCost, 1.1));
-    document.getElementById("AutoPrinterCost").innerHTML = NewAutoPrinterCost;
-    }
-    else {
-      money = money;
-      AutoPrinters = AutoPrinters
-    }
+  var element = document.body;
+  element.classList.toggle("theme-toggle");
 }
 
-//Buys an Auto Printer and activates it
- function BuyAndPrint() {
-  BuyAutoPrinters();
- };
+//reduces money by cost of printer and increments printer.
+var AutoPrinterCost = 10;
+var NewAutoPrinterCost;
+document.getElementById("AutoPrinterCost").innerHTML = AutoPrinterCost;
+function BuyAutoPrinter() {
+  if (money >= AutoPrinterCost) {
+    money = money - AutoPrinterCost;
+  }
+  else {
+    document.getElementbyId("CantAffordPrinter").innerHTML = "You can't afford an Auto Printer!";
+  }
+}
+
+
+
 
 
 
